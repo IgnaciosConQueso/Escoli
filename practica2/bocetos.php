3c50bf8a934f5668b6dc731150fa9f1f5e078758
aluque1 <aluquevillegas@gmail.com>
2023-03-14 12:17:08 +0100

    fuckit

--- practica2/bocetos.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'includes/config.php';
-
-$tituloPagina = 'Escoli';
-$contenidoPrincipal='/includes/vistas/contenidos/bocetos.php';
-
-$params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal];
-$app->generaVista('/plantillas/plantillaContenido.php', $params);
