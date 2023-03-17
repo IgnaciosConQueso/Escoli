012f830c5dc703861cc91ebae5fceacf5ae49fcf
pagarc24 <79839698+pagarc24@users.noreply.github.com>
2023-03-17 18:22:02 +0100

    borrando sobrantes

--- practica2/facultad.php
+++ practica2/facultad.php
@@ -6,7 +6,7 @@ require_once __DIR__.'/includes/vistas/helpers/facultad-helper.php';
 use function es\ucm\fdi\aw\listaValoraciones;
 
 $tituloPagina = 'Facultad';
-$contenidoPrincipal=listaValoraciones();
+$contenidoPrincipal = listaValoraciones();
 
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal];
 $app->generaVista('/plantillas/plantillaPrincipal.php', $params);
