9f10228f14f446505f0f0b5abf5e56a111a51fa8
Lem0z3n <guillermo.lemonnier@gmail.com>
2023-04-03 15:59:55 +0200

    hecho cosas de botones

--- practica2/facultad.php
+++ practica2/facultad.php
@@ -7,6 +7,7 @@ $tituloPagina = 'Facultad';
 $contenidoPrincipal = "<h1>Valoraciones de " . nombreFacultad($_GET['id']) . "</h1>";
 $contenidoPrincipal .= listaValoraciones($_GET['id']);
 
+
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal];
 $app->generaVista('/plantillas/plantillaPrincipal.php', $params);
 
