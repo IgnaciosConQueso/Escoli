6efb044415f3145166ffabbe41f216deabed7f74
Nachoski08 <nachosantatecla3@gmail.com>
2023-04-13 00:54:43 +0200

    Update registroUniversidad.php

--- practica2/registroUniversidad.php
+++ practica2/registroUniversidad.php
@@ -4,16 +4,17 @@ require_once __DIR__ . '/includes/config.php';
 
 $formRegistro = new \escoli\centros\FormularioUniversidad();
 
+$tituloPagina = 'Registro Universdad';
+
 if (isset($_GET['id']) && !isset($_POST['id'])) {
   $universidad = \escoli\centros\Universidad::buscaPorId($_GET['id']);
   $_POST['id'] = $universidad->id;
   $_POST['nombre'] = $universidad->nombre;
+  $tituloPagina = 'Modificación de Universidad';
 }
 
 $formRegistro = $formRegistro->gestiona();
 
-
-$tituloPagina = 'Registro Universdad';
 $contenidoPrincipal = <<<EOF
   	<h1>Registro de Universidad</h1>
     $formRegistro
