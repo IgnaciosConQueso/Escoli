17aeffaec4d70b9f3856b42a0df200f46899a71a
aluque1 <aluquevillegas@gmail.com>
2023-04-13 10:14:51 +0200

    funciona el perfil bien

--- practica2/includes/vistas/comun/sidebarIzq.php
+++ practica2/includes/vistas/comun/sidebarIzq.php
@@ -3,11 +3,7 @@ use escoli\Aplicacion;
 
 $app = Aplicacion::getInstance();
 ?>
-<nav id="sidebarIzq">
-	<h3>Navegación</h3>
-	<ul>
-		<li><a href="<?= $app->resuelve('/index.php') ?>">Inicio</a></li>
-		<li><a href="<?= $app->resuelve('/contenido.php') ?>">Ver contenido</a></li>
-		<li><a href="<?= $app->resuelve('/admin.php') ?>">Administrar</a></li>
-	</ul>
-</nav>
+
+<aside id="sidebarIzq">
+	<?= $params['contenidoSideBarIzq'] ?>
+</aside>
