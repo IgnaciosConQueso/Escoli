e7c602cb4526b407d0b4208ea3eac6e03a9b40e6
aluque1 <aluquevillegas@gmail.com>
2023-04-14 12:37:11 +0200

    linea de codigo sobrante

--- practica2/facultad.php
+++ practica2/facultad.php
@@ -12,8 +12,6 @@ $tituloPagina = 'Facultad';
 $contenidoPrincipal = "<h1>Valoraciones de " . nombreFacultad($_GET['id']) . "</h1>";
 $contenidoPrincipal .= listaValoraciones($_GET['id'], $app->resuelve('/facultad.php?id=' . $_GET['id']));
 
-$urlScript = $app->resuelve('/includes/src/contenido/likeScript.js');
-
 $params = ['tituloPagina' => $tituloPagina, 'scripts' => $urlScript, 'contenidoPrincipal' => $contenidoPrincipal];
 $app->generaVista('/plantillas/plantillaPrincipal.php', $params);
 
