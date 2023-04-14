1f133f535e0cfd0b2c6255ff4b7cc0bf2d1b4859
aluque1 <aluquevillegas@gmail.com>
2023-04-14 11:26:42 +0200

    refactorizacion de codigo

--- practica2/includes/vistas/helpers/facultad.php
+++ practica2/includes/vistas/helpers/facultad.php
@@ -1,5 +1,8 @@
 <?php
 
+
+require_once __DIR__ . '/includes/vistas/helpers/valoracion.php';
+
 use escoli\contenido\Valoracion;
 use escoli\centros\Facultad;
 use escoli\Aplicacion;
@@ -18,43 +21,6 @@ function listaValoraciones($id = 1, $numPorPag = 10, $pag = 1)
        
     }
     return $html;
-}
-
-function generaHTMLValoracion($valoracion)
-{
-    $html = '<li>';
-    $html .= '<div class="valoracion">';
-    $html .= '<p class="nombre-usuario">' . "idUsuario: " . $valoracion->idUsuario . '</p>';
-    $html .= '<p class="nombre-profesor">' . "idProfesor: " . $valoracion->idProfesor . '</p>';
-    $html .= '<p class="puntuacion">' . "puntuacion: " . $valoracion->puntuacion . '</p>';
-    $html .= '<p class="fecha">' . "fecha: " . $valoracion->fecha . '</p>';
-    $html .= '<p class="comentario">' . "comentario: " . $valoracion->comentario . '</p>';
-    $html .= '<p class="likes">' . "likes: " . $valoracion->likes . '</p>' ;
-    $html .= botonLike($valoracion->getId(), $valoracion->getLikes());
-    $html .= botonDislike($valoracion->getId(), $valoracion->getLikes());
-    $html .= '</div>';
-    $html .= '</li>';
-
-    return $html;
-}
-
-
-function botonLike($id,$likes)
-{
-    $likes++;
-    $idFac ='/facultad.php?id=' . $_GET['id'];
-    $app = Aplicacion::getInstance();
-    $api = $app->resuelve('/includes/src/contenido/api_likes.php');
-    return Formulario::buildButtonForm($api, ['id' => $id, 'likes' => $likes, 'envia' => $idFac] , '👍');
-}
-
-function botonDislike($id,$likes)
-{
-    $likes--;
-    $idFac = '/facultad.php?id=' . $_GET['id'];
-    $app = Aplicacion::getInstance();
-    $api = $app->resuelve('/includes/src/contenido/api_likes.php');
-    return Formulario::buildButtonForm($api, ['id' => $id, 'likes' => $likes, 'envia' => $idFac] , '👎');
 }
 
 function nombreFacultad($idFacultad)
