b6330edda468dba9f664cfadcba1155b7b206547
Jorbis21 <javierorbis@gmail.com>
2023-04-18 09:19:30 +0200

    Cambios aspecto

--- practica2/includes/vistas/comun/cabecera.php
+++ practica2/includes/vistas/comun/cabecera.php
@@ -56,7 +56,7 @@ function creaBarraBusqueda()
 <header>
     <div id="title">
         <a id="linkLogo" href="<?= $app->resuelve('index.php'); ?>"> <img id="logo" src="<?= $app->resuelve('/Imagenes/logo.png'); ?>" alt="logo" title="Escoli"></a>
-        <h1 id="headerTitle"><img id="banner" src="<?= $app->resuelve('/Imagenes/banner.png'); ?>" alt="banner" title="Escoli"></a></h1>
+        <h1 id="headerTitle"><img id="banner" src="<?= $app->resuelve('/Imagenes/letras.png'); ?>" alt="banner" title="Escoli"></a></h1>
         <div id="saludo">
             <?= mostrarSaludo(); ?>
         </div>
