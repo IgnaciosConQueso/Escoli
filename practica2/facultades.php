11196e6d144245ccfa4e9ae046119e64bba4dc49
Nachoski08 <nachosantatecla3@gmail.com>
2023-04-20 20:09:38 +0200

    Footer algo más elegante

--- practica2/facultades.php
+++ practica2/facultades.php
@@ -6,7 +6,7 @@ require_once __DIR__ . '/includes/vistas/helpers/facultadesHelper.php';
 $tituloPagina = 'Escoli';
 $contenidoPrincipal = "<h1>Facultades de " . nombreUniversidad($_GET['idUniversidad']) . "</h1>";
 $contenidoPrincipal .= listaFacultades($_GET['idUniversidad']);
-$menuCabecera = '<a href=' . $app->resuelve('/facultades.php?idUniversidad=' . $_GET['idUniversidad']) . '>Facultad</a>';
+$menuCabecera = '<a href=' . $app->resuelve('/facultades.php?idUniversidad=' . $_GET['idUniversidad']) . '>Facultades</a>';
 $_SESSION['menuCabecera']['anterior'] = $menuCabecera;
 
 $params = ['tituloPagina' => $tituloPagina, 'contenidoPrincipal' => $contenidoPrincipal, 'menuCabecera' => $menuCabecera];
--- practica2/includes/vistas/comun/pie.php
+++ practica2/includes/vistas/comun/pie.php
@@ -1,3 +1,15 @@
+<?php
+use escoli\Aplicacion;
+$app = Aplicacion::getInstance();
+?>
 <footer>
-	<p><a href="https://github.com/IgnaciosConQueso/Escoli">Source Code</a></p>
+	<div id="links">
+		<a href="<?= $app->resuelve('/contacto.php'); ?>">Contacto</a>
+		<a href="<?= $app->resuelve('/miembros.php'); ?>">Miembros</a>
+		<a href="<?= $app->resuelve('/detalles.php'); ?>">Detalles</a>
+	</div>
+	<div id="copy">
+		<p>© 2023 Escoli. All rights reserved.</p>
+		<a class="icons" href="https://github.com/IgnaciosConQueso/Escoli"> <img class="icons" src="<?= $app->resuelve('/Imagenes/github.png'); ?>" alt="github" title="Escoli"></a>
+	</div>
 </footer>
