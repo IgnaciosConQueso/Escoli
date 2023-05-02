997ca55ee8c940bdfe9f8233c1214ea0d13b1ebd
Lem0z3n <guillermo.lemonnier@gmail.com>
2023-05-02 22:33:56 +0200

    Avanzada refactorización de likes a JS

--- practica2/includes/vistas/helpers/api_likes.php
+++ practica2/includes/vistas/helpers/api_likes.php
@@ -8,25 +8,49 @@ require_once '../../config.php';
 
 $app = Aplicacion::getInstance();
 
-$idValoracion = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
+$idValoracion = filter_input(INPUT_POST, 'idValoracion', FILTER_SANITIZE_NUMBER_INT);
 $likes = filter_input(INPUT_POST, 'likes', FILTER_SANITIZE_NUMBER_INT);
-$origen = filter_input(INPUT_POST, 'url', FILTER_SANITIZE_URL);
 $valor = filter_input(INPUT_POST, 'valor', FILTER_SANITIZE_NUMBER_INT);
 
+$succes = true;
+$message = "";
 
 if(!isset($idValoracion)) {
     $app->paginaError(400, 'Error', 'Oops', 'No se ha recibido el id de la review');
 }
 
-if((!$app->idUsuario()) || (LikesyKarma ::checkLike($app->idUsuario(), $idValoracion, $valor))){
-    //compruebo que el que da like es un usuario. y que no le va a dar el mismo like.
-    $app->redirige($app->resuelve($origen));
+if((!$app->idUsuario())){
+    //compruebo que el que da like es un usuario.
+    $succes = false;
+    $message = "Debes iniciar sesión para dar likes";
+    $response = array("success" => $success, "message" => $message);
+    echo json_encode($response);
+    return;
 }
-
+if(LikesyKarma :: existeLike($app->idUsuario(), $idValoracion)){
+    $valorBD = LikesyKarma :: valorLike($app->idUsuario(), $idValoracion);
+    if($valorBD === 0){
+        $succes = false;
+        $message = "Algo feo feo esta pasando en missouri. (Error en la BD)";
+        $response = array("success" => $success, "message" => $message);
+        echo json_encode($response);
+        return;
+    } else if($valorBD === $valor){
+        $succes = false;
+        $message = "No puedes dar Like más de una vez chulopan.";
+        $response = array("success" => $success, "message" => $message);
+        echo json_encode($response);
+        return;
+    }
+}
 $likes += $valor;
-Valoracion::gestionaLikes($idValoracion, $likes);
+Valoracion::actualizaLikes($idValoracion, $likes);
 
-$app->redirige($app->resuelve($origen));
+$succes = true;
+$message = "Diste Like o diste dislike da igual porque no vas a ver esto. ;PP";
+$response = array("success" => $success, "message" => $message);
+echo json_encode($response);
+return;
 
 
 ?>
