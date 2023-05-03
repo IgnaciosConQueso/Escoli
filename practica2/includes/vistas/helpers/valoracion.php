a3da1b89e227be865eaafc2042a72de250e1dc68
aluque1 <aluquevillegas@gmail.com>
2023-05-03 13:40:57 +0200

    Revert "no hacia ninguna falta"
    This reverts commit 521c6db3e6ce1381ae1270130740553051e56070.

---
+++ practica2/includes/vistas/helpers/valoracion.php
@@ -0,0 +1,88 @@
+<?php
+
+use escoli\contenido\Valoracion;
+use escoli\Aplicacion;
+use escoli\Formulario;
+
+function listaValoracionesUsuario($id, $url)
+{
+    $arrayMensajes = Valoracion::buscaValoracionesPorIdUsuario($id);
+    $html = '';
+    if ($arrayMensajes) {
+        $html .= '<ul class="lista-valoraciones">';
+        foreach ($arrayMensajes as $valoracion) {
+            $html .= generaHTMLValoracion($valoracion, $url);
+
+        }
+        $html .= '</ul>';
+    }
+    return $html;
+}
+
+function listaTopCinco($id, $url)
+{
+    $arrayMensajes = Valoracion::buscaTopCincoValoraciones($id);
+    $html = '';
+    if ($arrayMensajes) {
+        $html .= '<ul class="lista-top5-valoraciones">';
+        foreach ($arrayMensajes as $valoracion) {
+            $html .= generaHTMLValoracion($valoracion, $url);
+
+        }
+        $html .= '</ul>';
+    }
+    return $html;
+}
+
+function listaNumeroDeLikes($id)
+{
+    $likes = Valoracion::listaNumeroDeLikes($id);
+    $html = '';
+    if ($likes) {
+        $html = generaHTMLLikesTotales($likes);
+    }
+    return $html;
+}
+function generaHTMLValoracion($valoracion, $url)
+{
+    $html = '<div class="valoracion">';
+    $html .= '<p class="nombre-usuario">' . "idUsuario: " . $valoracion->idUsuario . '</p>';
+    $html .= '<p class="nombre-profesor">' . "idProfesor: " . $valoracion->idProfesor . '</p>';
+    $html .= '<p class="puntuacion">' . "puntuacion: " . $valoracion->puntuacion . '</p>';
+    $html .= '<p class="fecha">' . "fecha: " . $valoracion->fecha . '</p>';
+    $html .= '<p class="comentario">' . "comentario: " . $valoracion->comentario . '</p>';
+    $html .= '<p class="likes">' . "likes: " . $valoracion->likes . '</p>';
+    $html .= botonLike($url, $valoracion->getId(), $valoracion->getLikes());
+    $html .= botonDislike($url, $valoracion->getId(), $valoracion->getLikes());
+    $html .= '</div>';
+    return $html;
+}
+
+function generaHTMLLikesTotales($likes)
+{
+    $html = '<div class="likes">';
+    $html .= '<p class="likes">' . $likes . '</p>';
+    $html .= '</div>';
+    return $html;
+}
+
+function botonLike($origen, $id, $likes)
+{
+    $valor = 1;
+    $app = Aplicacion::getInstance();
+    $api = $app->resuelve('/includes/vistas/helpers/api_likes.php');
+    return Formulario::buildButtonForm($api, 
+    ['url' => $origen, 'id' => $id, 'likes' => $likes, 'valor' => $valor],
+     '👍');
+}
+
+function botonDislike($origen, $id, $likes)
+{
+    $valor = -1;
+    $app = Aplicacion::getInstance();
+    $api = $app->resuelve('/includes/vistas/helpers/api_likes.php');
+    return Formulario::buildButtonForm($api, 
+    ['url' => $origen, 'id' => $id, 'likes' => $likes, 'valor' => $valor],
+     '👎');
+}
+?>
