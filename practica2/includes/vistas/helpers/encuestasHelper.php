810ab1f57749ee372eb10537dbb66cededd5b236
Nachoski08 <ignsan08@ucm.es>
2023-05-11 21:28:56 +0200

    Estilo encuestas

--- practica2/includes/vistas/helpers/encuestasHelper.php
+++ practica2/includes/vistas/helpers/encuestasHelper.php
@@ -4,6 +4,8 @@ use escoli\Aplicacion;
 use escoli\Formulario;
 use escoli\contenido\Encuesta;
 use escoli\contenido\CampoEncuesta;
+use escoli\usuarios\Usuario;
+use escoli\Imagen;
 
 function listaEncuestasFacultad($id, $url){
     $arrayEncuestas = Encuesta::buscaPorFacultad($id);
@@ -21,15 +23,25 @@ function listaEncuestasFacultad($id, $url){
 
 function generaHTMLEncuesta($encuesta, $url){
     $campos = CampoEncuesta::buscaPorEncuesta($encuesta->id);
+    $usuario = Usuario::buscaPorId($encuesta->idUsuario);
+    $imagen = Imagen::buscaPorId($usuario->idImagen);
+    $htmlImg = '<img class="imagen-usuario" src="' . Aplicacion::getInstance()->resuelveImagen($imagen->ruta) . '" alt="Imagen de usuario">';
 
     $html = '<li class="encuesta">';
+    $html .= '<div class="header-encuesta">';
+    $html .= '<a class="imagen-usuario" href="' . Aplicacion::getInstance()->resuelve('perfilAlumno.php?id=' . $usuario->id) . '">' . $htmlImg . '</a>';
+    $html .= '<div class="info-encuesta">';
+    $html .= '<p class="usuario-encuesta">' . $usuario->nombreUsuario;
     $html .= '<p class="titulo-encuesta">' . $encuesta->titulo . '</p>';
+    $html .= '</div>';
+    $html .= '</div>';
     foreach ($campos as $campo) {
         $html .= '<div class="campo-encuesta">';
         $html .= botonCampoEncuesta($encuesta, $campo, $url);
         $html .= '<p class="votos-encuesta">' . $campo->votos . '</p>';
         $html .= '</div>';
     }
+    $html .= '<p class="fecha-encuesta">' . $encuesta->fecha . '</p>';
     $html .= '</li>';
 
     return $html;
